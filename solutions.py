ae20fd61db38dacd10ae291b6bb203151936b70c
Ricky <rbedia17@googlemail.com>
2022-11-28 14:04:11 +0000

    Add solution to problem 2

--- solutions.py
+++ solutions.py
@@ -4,3 +4,14 @@ def problem_1(n):
         if i % 3 == 0 or i % 5 == 0:
             total += i
     return total
+
+def problem_2(n):
+    l = [0, 1]
+    next_value = sum(l[-2:])
+    total = 0
+    while next_value < n:
+        if next_value % 2 == 0:
+            total += next_value
+        l.append(next_value)
+        next_value = sum(l[-2:])
+    return total
