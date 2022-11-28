2ace46c0c83e8e2c1fa49d9e2db306c1938c1f82
Ricky <rbedia17@googlemail.com>
2022-11-28 14:13:17 +0000

    Add solution to problem 3

--- solutions.py
+++ solutions.py
@@ -5,6 +5,9 @@ def problem_1(n):
             total += i
     return total
 
+# ============================================================
+# ============================================================ 
+
 def problem_2(n):
     l = [0, 1]
     next_value = sum(l[-2:])
@@ -15,3 +18,33 @@ def problem_2(n):
         l.append(next_value)
         next_value = sum(l[-2:])
     return total
+
+# ============================================================
+# ============================================================ 
+
+def modulo(divider, n):
+    if n % divider == 0:
+        return int(n / divider)
+    else:
+        return n
+    
+def smallest_factor(n):
+    i = 2
+    res = modulo(i, n)
+    while res == n:
+        i += 1
+        res = modulo(i, n)
+    return i, res
+
+def problem_3(n):
+    factors = []
+    factor, res = smallest_factor(n)
+    factors.append(factor)
+    while res != 1:
+        factor, res = smallest_factor(res)
+        factors.append(factor)
+    return factors
+
+# ============================================================
+# ============================================================ 
+
