eb841600e0acbe4621b52b4af9756dfffc561eea
Ricky <rbedia17@googlemail.com>
2022-11-29 10:11:57 +0000

    Add solution to problem 4

--- solutions.py
+++ solutions.py
@@ -1,23 +1,16 @@
-def problem_1(n):
-    total = 0
-    for i in range(1, n):
-        if i % 3 == 0 or i % 5 == 0:
-            total += i
-    return total
-
-# ============================================================
-# ============================================================ 
-
-def problem_2(n):
-    l = [0, 1]
-    next_value = sum(l[-2:])
-    total = 0
-    while next_value < n:
-        if next_value % 2 == 0:
-            total += next_value
-        l.append(next_value)
-        next_value = sum(l[-2:])
-    return total
+def problem_4(start, end):
+    largest_i = 0
+    largest_j = 0
+    largest_n = 0
+    for i in range(start, end):
+        for j in range(start, end):
+            n = i * j
+            reversed_n = int(str(n)[::-1])
+            if n == reversed_n and n > largest_n:
+                largest_i = i
+                largest_j = j
+                largest_n = n
+    return largest_i, largest_j, largest_n
 
 # ============================================================
 # ============================================================ 
@@ -48,3 +41,23 @@ def problem_3(n):
 # ============================================================
 # ============================================================ 
 
+def problem_2(n):
+    l = [0, 1]
+    next_value = sum(l[-2:])
+    total = 0
+    while next_value < n:
+        if next_value % 2 == 0:
+            total += next_value
+        l.append(next_value)
+        next_value = sum(l[-2:])
+    return total
+
+# ============================================================
+# ============================================================ 
+
+def problem_1(n):
+    total = 0
+    for i in range(1, n):
+        if i % 3 == 0 or i % 5 == 0:
+            total += i
+    return total
